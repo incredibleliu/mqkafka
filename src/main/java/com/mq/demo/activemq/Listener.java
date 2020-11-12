00a8c01f6395ef8e4b40e40df101d4685b841c37
incredibleliu <incredibleliu@gmail.com>
2020-11-13 03:12:42 +0800

    bitronix

--- src/main/java/com/mq/demo/activemq/Listener.java
+++ src/main/java/com/mq/demo/activemq/Listener.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.jms.JMSException;
@@ -21,7 +22,8 @@ public class Listener {
 
     @JmsListener(destination = "inbound.queue")
     //@Transactional(rollbackFor = {CheckedException.class}, timeout = 100000, value = "jtaTransactionManager")
-    @Transactional(rollbackFor = {RuntimeException.class}, timeout = 100000, value = "platformTransactionManager")
+    //@Transactional(rollbackFor = {RuntimeException.class}, timeout = 100000, value = "platformTransactionManager")
+    @Transactional(timeout = 100000, value = "platformTransactionManager", propagation = Propagation.REQUIRED)
     public String receiveMessage(final Message jsonMessage) throws JMSException {
         String messageData = null;
         String response = "ok";
--- src/main/java/com/mq/demo/activemq/Service.java
+++ src/main/java/com/mq/demo/activemq/Service.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Slf4j
@@ -12,6 +13,7 @@ public class Service {
     @Retryable( value = RuntimeException.class,
                 maxAttempts = 3,
                 backoff = @Backoff(delay = 10000) )
+    @Transactional
     public void execute(){
 
             log.info("### retry...");
@@ -23,9 +25,15 @@ public class Service {
 //                e.printStackTrace();
 //            }
 
+            try{
+                throw new CheckedException();
+            } catch(Throwable e){
+                System.out.println("### caught CheckedException ...");
+            }
+
             //throw new NullPointerException();
 
-            NotComponent.execute();
+            //NotComponent.execute();
 
     }
 
